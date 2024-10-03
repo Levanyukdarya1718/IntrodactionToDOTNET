f67254c7d5d35873f5438b5cef85898f9b6f9076
Дашка Леванюк <levanyuk.darja@yandex.ru>
2024-10-03 20:54:32 +0500

    HomeworkFactorial

---
+++ DataTypes/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypes
+{
+	internal class Program
+	{
+		static readonly string delimeter = "\n--------------------------------------------\n";
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello Data Types");
+			 
+			//Console.WriteLine(sizeof(bool));
+			//Console.WriteLine(true);
+			//Console.WriteLine(false);
+			//Console.WriteLine(Boolean.FalseString);
+
+			Console.WriteLine('+'.GetType());
+			Console.WriteLine(sizeof(byte));
+			Console.WriteLine(byte.MinValue);
+			Console.WriteLine(byte.MaxValue);
+			Console.WriteLine(delimeter);
+
+			Console.WriteLine("SByte:");
+			Console.WriteLine(sizeof(sbyte));
+			Console.WriteLine(sbyte.MinValue);
+			Console.WriteLine(sbyte.MaxValue);
+			Console.WriteLine(delimeter);
+
+			Console.WriteLine("Decimal:");
+			Console.WriteLine(sizeof(decimal));
+			Console.WriteLine(decimal.MinValue);
+			Console.WriteLine(decimal.MaxValue);
+
+			Console.WriteLine(5UL.GetType());
+			int a = 2000000000;
+			int b = 4;
+
+	
+			Console.WriteLine((a*b).GetType());
+			Console.WriteLine("Вот и сказочке конец");
+
+
+
+		}
+	}
+}
---
+++ HomeworkFactorial/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+
+namespace HomeworkFactorial
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int n;
+			int t = 1;
+			Console.Write("Введите число:");
+			n = Convert.ToInt32(Console.ReadLine());
+			for (int i = 1; i <= n; i++)
+			{
+				t *= i;
+			}
+			Console.WriteLine(t);
+
+			//BigInteger k;
+			//for (int i = 1; i <= n; i++)
+			//{
+			//	k *= i;
+			//}
+			do
+			{
+				double a, b;
+				char symbol;
+				double result = 0;
+				Console.Write("Введите число a:");
+				 a =Convert.ToDouble(Console.ReadLine());
+
+				Console.Write("Введите число b:");
+				b = Convert.ToDouble(Console.ReadLine());
+
+				Console.Write("Введите символ +, =, *, / ");
+				symbol= Console.ReadKey().KeyChar;
+
+				switch(symbol)
+				{
+					case '+':
+						result= a + b;
+						break;
+					case '-':
+						result = a - b;
+						break;
+					case '*':
+						result= a * b;
+						break;
+					case '/':
+						if(b!=0)
+						{
+							result =a/ b;
+						}
+						else
+						{
+							Console.WriteLine("Делить на 0 нельзя!");
+						}
+						break;
+					
+
+
+				}
+				Console.WriteLine("\n");
+				Console.WriteLine($"{ a}{ symbol}{ b}={ result}");
+			} while (true);
+			
+
+		}
+	}
+}
