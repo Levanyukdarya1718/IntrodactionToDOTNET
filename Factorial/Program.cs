0e7daf8eede15b1c68e68051c8c9231bbebca16b
Дашка Леванюк <levanyuk.darja@yandex.ru>
2024-10-03 21:41:21 +0500

    FactorialDONE

---
+++ Factorial/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace Factorial
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("Введите число для вычисления факториала:");
+			int n = Convert.ToInt32(Console.ReadLine());
+			BigInteger f = 1;
+			try
+			{
+				for (int i = 1; i <= n; i++)
+				{
+					f *= i;
+					Console.WriteLine($"{i}!={f}");
+				}
+			}
+			catch (Exception ex)
+			{
+
+				Console.WriteLine(ex.Message);
+			}
+			Console.WriteLine("Fenira la comedia");
+		}
+	}
+}
