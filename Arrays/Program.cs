c7a36048c7bac45ff8a21698392915db336b354f
Дашка Леванюк <levanyuk.darja@yandex.ru>
2024-10-08 18:10:37 +0500

    HomeworkARRAY

---
+++ Arrays/Program.cs
@@ -0,0 +1,127 @@
+﻿#define ARRAYS_1
+#define ARRAYS_2
+#define JAGGED_ARRAY
+using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Linq; //предоставляет методы для выполнения операций над массивом.
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+	internal class Program
+	{
+		static readonly string delimiter = "\n--------------------------------------\n";
+		static void Main(string[] args)
+		{
+			Random rand = new Random(0);
+#if ARRAYS_1
+			Console.Write("Введите размер мавссива:");
+			int n= Convert.ToInt32(Console.ReadLine());
+			//int[] arr =  { 3, 5, 8, 13, 21 };
+			int[] arr = new int[n];
+			//Random rand= new Random(0);
+			for(int i=0; i<arr.Length; i++)
+			{
+				arr[i] = rand.Next(100);
+			}
+			//for(int i=0; i<arr.Length; i++)
+			//{
+			//	Console.Write(arr[i] + "\t");
+			//}
+			//Console.WriteLine();
+
+			foreach (int i in arr)
+			{
+				Console.Write(i + "\t");
+			}
+			Console.WriteLine();
+			Console.WriteLine(delimiter);
+			int sum_array1 = arr.Sum();
+			Console.WriteLine($"сумма элементов одномерного массива: {sum_array1}");
+			double avg_array1 = arr.Average();
+			Console.WriteLine($"Седнее аифметичепское одномеронго массива: {avg_array1}");
+			int min_array1 = arr.Min();
+			Console.WriteLine($"Минимальное значение одномерного массива:{min_array1}");
+			int max_array1 = arr.Max();
+			Console.WriteLine($"Максимальное значение одномерного мкассива: {max_array1}");
+			Console.WriteLine(delimiter);
+			foreach (int i in arr)
+			{
+				Array.Sort(arr);
+				Console.Write(i+"\t");
+			}
+			Console.WriteLine();
+			Console.WriteLine(delimiter);
+#endif
+
+#if ARRAYS_2
+			Console.Write("Введите количество строк: ");
+			int rows = Convert.ToInt32(Console.ReadLine());
+
+			Console.Write("Введите количество элементов строки: ");
+			int cols = Convert.ToInt32(Console.ReadLine());
+
+			int[,] i_arr_2 = new int[rows, cols];
+
+			//Console.WriteLine(i_arr_2.Rank);
+
+			for (int i = 0; i < i_arr_2.GetLength(0); i++)
+			{
+				for (int j = 0; j < i_arr_2.GetLength(1); j++)
+				{
+					i_arr_2[i, j] = rand.Next(100);
+				}
+			}
+			for (int i = 0; i < i_arr_2.GetLength(0); i++)
+			{
+				for (int j = 0; j < i_arr_2.GetLength(1); j++)
+				{
+					Console.Write(i_arr_2[i, j] + "\t");
+				}
+				Console.WriteLine();
+			}
+			Console.WriteLine(delimiter);
+			int sum_array2 = i_arr_2.Cast<int>().Sum();
+			Console.WriteLine($"Сумма эхлементов двумерного массива: {sum_array2}");
+			double avg_array2= i_arr_2.Cast<int>().Average();
+			Console.WriteLine($"Среднеарифметическое элементов двумерного массива: {avg_array2}");
+			int min_array2 = i_arr_2.Cast<int>().Min();
+			Console.WriteLine($"Минимальное значение элементов двумерного массива: {min_array2}");
+			int max_array2 = i_arr_2.Cast<int>().Max();
+			Console.WriteLine($"Максимальное значение элементов двумерного массива: {max_array2}");
+			Console.WriteLine(delimiter);
+#endif
+
+#if JAGGED_ARRAY
+			int[][] arr_jagged = new int[][]
+			{
+				new int []{0, 1, 1, 2 },
+				new int []{3, 5, 8, 13, 21 },
+				new int []{ 34, 55, 89 },
+				new int []{144, 233, 377, 610, 987 }
+			};
+			for (int i = 0; i < arr_jagged.Length; i++)
+			{
+				for (int j = 0; j < arr_jagged[i].Length; j++)
+				{
+					Console.Write(arr_jagged[i][j] + "\t");
+				}
+				Console.WriteLine();
+			}
+			Console.WriteLine(delimiter);
+			int sum_arrayJagged = arr_jagged.SelectMany(x => x).Sum();
+			Console.WriteLine($"Сумма элементов зубчатого массива: {sum_arrayJagged}");
+			double avg_arrayJagged = arr_jagged.SelectMany(x=>x).Average();
+			Console.WriteLine($"Среднее арифметическое элементов зубчатого массива: {avg_arrayJagged}");
+			int min_arrayJagged = arr_jagged.SelectMany(x => x).Min();
+			Console.WriteLine($"Минимальное значение элементо зубчатого массива: {min_arrayJagged}");
+			int max_arrayJagged = arr_jagged.SelectMany(x=>x ).Max();
+			Console.WriteLine($"Максимальное значение элементов зубчатого массива: {max_arrayJagged}");
+#endif
+		}
+	}
+}
--- Snake/Program.cs
+++ Snake/Program.cs
@@ -14,69 +14,173 @@ namespace Snake
 	{
 		static void Main(string[] args)
 		{
-			//int x = 20, y = 10;
-			//int posX = 80;
-			//int posY = 26;
-			Console.BufferHeight = Console.WindowHeight;
-			Console.BufferWidth = Console.WindowWidth;
-			Console.CursorVisible = false;
-			Random rnd = new Random();
-			//int x = rnd.Next(posX);
-			//int y = rnd.Next(posY);
-			int x = rnd.Next(Console.BufferWidth);
-			int y = rnd.Next(Console.BufferHeight);
-			Console.SetCursorPosition(x, y);
-			
-			char symbol = '*';
-			
-		
-			Console.WriteLine(symbol);
-			//Console.Write(symbol);
+			////int x = 20, y = 10;
+			////int posX = 80;
+			////int posY = 26;
+			//Console.BufferHeight = Console.WindowHeight;
+			//Console.BufferWidth = Console.WindowWidth;
+			//Console.CursorVisible = false;
+			//Random rnd = new Random();
+			////int x = rnd.Next(posX);
+			////int y = rnd.Next(posY);
+			//int x = rnd.Next(Console.BufferWidth);
+			//int y = rnd.Next(Console.BufferHeight);
+			//Console.SetCursorPosition(x, y);
+
+			//char symbol = '*';
+
+
+			//Console.WriteLine(symbol);
+			////Console.Write(symbol);
+			//ConsoleKey key;
+			//do
+			//{
+			//	key = Console.ReadKey(true).Key;
+			//	switch (key)
+			//	{
+			//		case ConsoleKey.S:
+
+			//		case ConsoleKey.DownArrow:
+			//			if (y < Console.BufferHeight - 1)
+			//				y++; break;
+			//		case ConsoleKey.W:
+			//			if (y > 0)
+			//				y--; break;
+			//		case ConsoleKey.UpArrow:
+			//			if (y > 0)
+			//				y--; break;
+			//		case ConsoleKey.A:
+			//			if (x > 1)
+			//				x--; break;
+			//		case ConsoleKey.LeftArrow:
+			//			if (x > 1)
+			//				x--; break;
+			//		case ConsoleKey.D:
+			//			if (x < Console.BufferWidth - 1)
+			//				x++; break;
+			//		case ConsoleKey.RightArrow:
+			//			if (x < Console.BufferWidth - 1)
+			//				x++; break;
+			//	}
+			//	//if (x < 0) x = 0;
+			//	//if (y < 1) y = 1;
+			//	////if (x > posX - 1) x = posX - 1;
+			//	////if (y > posY - 1) y = posY - 1;
+			//	//if (x > Console.BufferWidth-1) x = Console.BufferWidth-1;
+			//	//if (y > Console.BufferHeight-1) y = Console.BufferHeight-1;
+
+			//	Console.Clear();
+			//	Console.ResetColor();
+			//	Console.WriteLine($"{(int)key}\t{key}");
+			//	Console.SetCursorPosition(x, y);
+			//	Console.Write(symbol);
+
+
+			//} while (key != ConsoleKey.Escape);         ////int x = 20, y = 10;
+			//											//int posX = 80;
+			//											//int posY = 26;
+			//Console.BufferHeight = Console.WindowHeight;
+			//Console.BufferWidth = Console.WindowWidth;
+			//Console.CursorVisible = false;
+			//Random rnd = new Random();
+			////int x = rnd.Next(posX);
+			////int y = rnd.Next(posY);
+			//int x = rnd.Next(Console.BufferWidth);
+			//int y = rnd.Next(Console.BufferHeight);
+			//Console.SetCursorPosition(x, y);
+
+			//char symbol = '*';
+
+
+			//Console.WriteLine(symbol);
+			////Console.Write(symbol);
+			//ConsoleKey key;
+			//do
+			//{
+			//	key = Console.ReadKey(true).Key;
+			//	switch (key)
+			//	{
+			//		case ConsoleKey.S:
+
+			//		case ConsoleKey.DownArrow:
+			//			if (y < Console.BufferHeight - 1)
+			//				y++; break;
+			//		case ConsoleKey.W:
+			//			if (y > 0)
+			//				y--; break;
+			//		case ConsoleKey.UpArrow:
+			//			if (y > 0)
+			//				y--; break;
+			//		case ConsoleKey.A:
+			//			if (x > 1)
+			//				x--; break;
+			//		case ConsoleKey.LeftArrow:
+			//			if (x > 1)
+			//				x--; break;
+			//		case ConsoleKey.D:
+			//			if (x < Console.BufferWidth - 1)
+			//				x++; break;
+			//		case ConsoleKey.RightArrow:
+			//			if (x < Console.BufferWidth - 1)
+			//				x++; break;
+			//	}
+			//	//if (x < 0) x = 0;
+			//	//if (y < 1) y = 1;
+			//	////if (x > posX - 1) x = posX - 1;
+			//	////if (y > posY - 1) y = posY - 1;
+			//	//if (x > Console.BufferWidth-1) x = Console.BufferWidth-1;
+			//	//if (y > Console.BufferHeight-1) y = Console.BufferHeight-1;
+
+			//	Console.Clear();
+			//	Console.ResetColor();
+			//	Console.WriteLine($"{(int)key}\t{key}");
+			//	Console.SetCursorPosition(x, y);
+			//	Console.Write(symbol);
+
+
+			//} while (key != ConsoleKey.Escape);
+			//int x = 10;
+			//int y = 10;
+
 			ConsoleKey key;
+			Console.SetBufferSize(Console.WindowWidth, Console.WindowHeight);
+			Random rand = new Random();
+			int x = rand.Next(Console.WindowWidth);
+			int y = rand.Next(Console.WindowHeight);
+			Console.CursorVisible = false;
+			//Console.BufferWidth= Console.WindowWidth;
+			//Console.BufferHeight= Console.WindowHeight;
 			do
 			{
-				key = Console.ReadKey(true).Key;
-					switch(key)
-					{
-					case ConsoleKey.S :
-						
-					case ConsoleKey.DownArrow:
-						if (y < Console.BufferHeight - 1)
-							y++; break;
-					case ConsoleKey.W:
-						if(y>0)
-						y--; break;
-					case ConsoleKey.UpArrow: 
-						if (y > 0)
-							y--; break;
-					case ConsoleKey.A: 
-						if(x>1)
-						x --; break;
-					case ConsoleKey.LeftArrow:
-						if (x > 1)
-							x--; break;
-					case ConsoleKey.D: 
-						if(x<Console.BufferWidth-1)
-							x ++; break;
-					case ConsoleKey.RightArrow:
-						if (x < Console.BufferWidth - 1)
-							x++; break;
-				}
-				//if (x < 0) x = 0;
-				//if (y < 1) y = 1;
-				////if (x > posX - 1) x = posX - 1;
-				////if (y > posY - 1) y = posY - 1;
-				//if (x > Console.BufferWidth-1) x = Console.BufferWidth-1;
-				//if (y > Console.BufferHeight-1) y = Console.BufferHeight-1;
 				
 				Console.Clear();
+				Console.SetCursorPosition(0,0);
 				Console.ResetColor();
-				Console.WriteLine($"{(int)key}\t{key}");
+				Console.Write($"x={x}\tY={y}");
 				Console.SetCursorPosition(x, y);
-				Console.Write(symbol);
-				
+				Console.ForegroundColor = ConsoleColor.Yellow;
+				Console.WriteLine((char)2);
+				key = Console.ReadKey(true).Key;
+				switch (key)
+				{
+					case ConsoleKey.UpArrow:
+					case ConsoleKey.W: y--; break;
+					case ConsoleKey.DownArrow:
+					case ConsoleKey.S: y++; break;
+					case ConsoleKey.LeftArrow:
+					case ConsoleKey.A: x--; break;
+					case ConsoleKey.RightArrow:
+					case ConsoleKey.D: x++; break;
+				}
+				if (x == 0 || y == 0 || 
+					x == Console.WindowWidth || y == Console.WindowHeight
+					) Console.Beep();
+				if (x == 0) x = 1;
+				if (x == Console.WindowWidth) x = Console.WindowWidth - 1;
+				if (y == 0) y = 1;
+				if (y == Console.WindowHeight) y = Console.WindowHeight - 1;
 
-			} while (key != ConsoleKey.Escape);
+			}while(key != ConsoleKey.Escape);
 			
 		}
 	}
